8d15af42144dfc1f878827e5995d92f03d634d47
Vitaliy Sobur <vitaliysobur@fb.com>
2022-09-12 20:45:13 +0200

    + CeloProvider

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false,
+            net: false
+        }
+    }
+
+    return config;
+}
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -2,78 +2,94 @@ import React, { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import s from '../styles/App.module.css'
+import { CeloProvider } from '@celo/react-celo';
+import '@celo/react-celo/lib/styles.css';
 
-export default function App() {
+export default function WrappedApp() {
+  return (
+    <CeloProvider
+      dapp={{
+        name: 'My awesome dApp',
+        description: 'My awesome description',
+        url: 'https://example.com',
+      }}
+    >
+      <App/>
+    </CeloProvider>
+  )
+}
+
+function App() {
   const [nav, setNav] = useState(0); // 0 - "pay", 1 - "register"
 
   return (
     <div className={`${s.container} ${nav && s.darkBg}`}>
-      <Head>
-        <title>CarbonPay</title>
-        <meta name="description" content="Fight climate change by doing whatever you do best" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Head>
+      <title>CarbonPay</title>
+      <meta name="description" content="Fight climate change by doing whatever you do best" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main className={`${s.main}`}>
-        <header className={s.header}>
-          <div className={s.headerInner}>
-            <div className={s.logo}>
-              <Image src="/carbonpay-logo.png" width="256px" height="64px" layout="fixed" />
-            </div>
-            <div className={s.btnSection}><button className={s.btn}>Connect Wallet</button></div>
+    <main className={`${s.main}`}>
+      <header className={s.header}>
+        <div className={s.headerInner}>
+          <div className={s.logo}>
+            <Image src="/carbonpay-logo.png" width="256px" height="64px" layout="fixed" />
           </div>
-        </header>
-        <div className={s.content}>
-          <ul className={s.nav}>
-            <li className={`${s.navItem} ${s.navItemFirst} ${!nav && s.navItemSelected}`}><a onClick={() => setNav(0)} href="#pay">Pay</a></li>
-            <li className={`${s.navItem} ${nav && s.navItemSelected}`}><a href="#register" onClick={() => setNav(1)}>Register Merchant</a></li>
-          </ul>
-
-          {!nav ? <div className={s.formWrap}>
-            <div className={s.formControl}>
-              <label className={s.label}>Merchant ID</label>
-              <div className={s.inputWrap}>
-                <input placeholder="0x..." className={s.input} type="text" />
-                <div className={s.subLabel}>
-                  Jen's Bakery
-                </div>
-              </div>
-            </div>
-            <div className={s.formControl}>
-              <label className={s.label}>Payment Amount</label>
-              <div className={s.inputWrap}>
-                <input placeholder="0" className={s.input} type="text" />
-                <div className={s.subLabel}>
-                  $4.79 USD
-                </div>
-              </div>
-            </div>
-            <div className={s.formControl}>
-              <label className={s.label}>Gas Fee</label>
-              <div className={`${s.subLabel} ${s.subLabelLarge}`}>+0.05 CELO</div>
-            </div>
-            <button className={`${s.btn} ${s.btnLarge}`}>Authorise Transaction</button>
-          </div>
-            :
-          <div className={s.formWrap}>
-            <div className={s.formControl}>
-              <label className={s.label}>Merchant Name</label>
-              <div className={s.inputWrap}>
-                <input placeholder="Jen's Bakery" className={s.input} type="text" />
-              </div>
-            </div>
-            <div className={s.formControl}>
-              <label className={s.label}>Minting Fee</label>
-              <div className={`${s.subLabel} ${s.subLabelLarge}`}>+5 CELO</div>
-            </div>
-            <div className={s.formControl}>
-              <label className={s.label}>Gas Fee</label>
-              <div className={`${s.subLabel} ${s.subLabelLarge}`}>+0.05 CELO</div>
-            </div>
-            <button className={`${s.btn} ${s.btnLarge}`}>Mint carbonNFT</button>
-          </div>}
+          <div className={s.btnSection}><button className={s.btn}>Connect Wallet</button></div>
         </div>
-      </main>
-    </div>
+      </header>
+      <div className={s.content}>
+        <ul className={s.nav}>
+          <li className={`${s.navItem} ${s.navItemFirst} ${!nav && s.navItemSelected}`}><a onClick={() => setNav(0)} href="#pay">Pay</a></li>
+          <li className={`${s.navItem} ${nav && s.navItemSelected}`}><a href="#register" onClick={() => setNav(1)}>Register Merchant</a></li>
+        </ul>
+
+        {!nav ? <div className={s.formWrap}>
+          <div className={s.formControl}>
+            <label className={s.label}>Merchant ID</label>
+            <div className={s.inputWrap}>
+              <input placeholder="0x..." className={s.input} type="text" />
+              <div className={s.subLabel}>
+                Jen's Bakery
+              </div>
+            </div>
+          </div>
+          <div className={s.formControl}>
+            <label className={s.label}>Payment Amount</label>
+            <div className={s.inputWrap}>
+              <input placeholder="0" className={s.input} type="text" />
+              <div className={s.subLabel}>
+                $4.79 USD
+              </div>
+            </div>
+          </div>
+          <div className={s.formControl}>
+            <label className={s.label}>Gas Fee</label>
+            <div className={`${s.subLabel} ${s.subLabelLarge}`}>+0.05 CELO</div>
+          </div>
+          <button className={`${s.btn} ${s.btnLarge}`}>Authorise Transaction</button>
+        </div>
+          :
+        <div className={s.formWrap}>
+          <div className={s.formControl}>
+            <label className={s.label}>Merchant Name</label>
+            <div className={s.inputWrap}>
+              <input placeholder="Jen's Bakery" className={s.input} type="text" />
+            </div>
+          </div>
+          <div className={s.formControl}>
+            <label className={s.label}>Minting Fee</label>
+            <div className={`${s.subLabel} ${s.subLabelLarge}`}>+5 CELO</div>
+          </div>
+          <div className={s.formControl}>
+            <label className={s.label}>Gas Fee</label>
+            <div className={`${s.subLabel} ${s.subLabelLarge}`}>+0.05 CELO</div>
+          </div>
+          <button className={`${s.btn} ${s.btnLarge}`}>Mint carbonNFT</button>
+        </div>}
+      </div>
+    </main>
+  </div>
   )
 }
