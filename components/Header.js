23eac4c77601664578b14ab88271eac59441b0d8
Vitaliy Sobur <vitaliysobur@fb.com>
2022-09-14 22:22:36 +0200

    + link to logo

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,11 @@ export default ({
   return (
     <header className={s.header}>
       <div className={s.headerInner}>
-        <div className={s.logo}>
-          <Image src="/carbonpay-logo.png" width="256px" height="64px" layout="fixed" />
-        </div>
+        <Link href="/">
+          <a className={s.logo}>
+            <Image src="/carbonpay-logo.png" width="256px" height="64px" layout="fixed" />
+          </a>
+        </Link>
         {!address && <div className={s.btnSection}><button onClick={connect} className={s.btn}>Connect Wallet</button></div>}
         {registered && <Link href="/merchant"><a className={s.profileLink}>Profile</a></Link>}
       </div>
