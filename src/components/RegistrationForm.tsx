7bf1001270457ec2930634c5bf829fc8c34e58ba
dresha-dev <ponamarevandrej@gmail.com>
2022-10-12 21:56:51 +0200

    feat: Use formik for registration form

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,64 +1,75 @@
-import React, { useRef, useCallback, useContext } from "react";
+import React, { useCallback, useContext } from "react";
 import { useRouter } from "next/router";
 import { useCelo } from "@celo/react-celo";
 import { nftContract } from "@/services/contracts";
 import { WalletContext } from "@/context/WalletContext";
 import Form from "@/components/Form";
 import useGas from "@/hooks/useGas";
+import { Formik, Field, ErrorMessage } from "formik";
 
 const RegistrationForm = () => {
   const { address, connect } = useContext(WalletContext);
   const { kit } = useCelo();
   const { gas } = useGas();
-  const merchantInput = useRef<HTMLInputElement>(null);
   const router = useRouter();
 
-  const handleRegister = useCallback(async () => {
-    const name = merchantInput?.current?.value;
-    if (!name) {
-      return;
-    }
-
-    try {
-      await nftContract(kit)
-        .methods.safeMint(address, name)
-        .send({ from: address });
-      router.push("/profile");
-    } catch (err) {
-      console.log(err);
-      if (/4001/.test(err as string)) {
-        console.log("Rejected");
-      } else {
-        !address && (await connect());
+  const handleRegister = useCallback(
+    async (merchantName: string) => {
+      try {
+        await nftContract(kit)
+          .methods.safeMint(address, merchantName)
+          .send({ from: address });
+        router.push("/profile");
+      } catch (err) {
+        console.log(err);
+        if (/4001/.test(err as string)) {
+          console.log("Rejected");
+        } else {
+          !address && (await connect());
+        }
       }
-    }
-  }, [address, connect, kit, router]);
+    },
+    [address, connect, kit, router]
+  );
 
   return (
-    <Form>
-      <div className="formControl">
-        <label className="label">Merchant Name</label>
-        <div className="inputWrap">
-          <input
-            ref={merchantInput}
-            placeholder="Jen's Bakery"
-            className="input"
-            type="text"
+    <Formik
+      initialValues={{ merchantName: "" }}
+      onSubmit={async (values) => {
+        await handleRegister(values.merchantName);
+      }}
+    >
+      <Form>
+        <div className="formControl">
+          <label className="label">Merchant Name</label>
+          <div className="inputWrap">
+            <Field
+              name="merchantName"
+              placeholder="Jen's Bakery"
+              className="input"
+              type="text"
+              validate={(value: string) => (!value ? "Required" : undefined)}
+            />
+          </div>
+          <ErrorMessage
+            name="merchantName"
+            component="div"
+            className="text-red-500"
           />
         </div>
-      </div>
-      {/* <div className="formControl">
+        {/* <div className="formControl">
         <label className="label">Minting Fee</label>
         <div className="subLabel subLabelLarge">+5 CELO</div>
       </div> */}
-      <div className="formControl">
-        <label className="label">Gas Fee</label>
-        <div className="subLabel subLabelLarge">+ {gas} CELO</div>
-      </div>
-      <button onClick={handleRegister} className="w-full text-lg btn-primary">
-        Mint carbonNFT
-      </button>
-    </Form>
+        <div className="formControl">
+          <label className="label">Gas Fee</label>
+          <div className="subLabel subLabelLarge">+ {gas} CELO</div>
+        </div>
+        <button type="submit" className="w-full text-lg btn-primary">
+          Mint carbonNFT
+        </button>
+      </Form>
+    </Formik>
   );
 };
 
