26f4758890360fdca48302b05332898647ae9650
dresha-dev <ponamarevandrej@gmail.com>
2022-10-12 21:58:02 +0200

    refactor: Remove unused imports

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useState } from "react";
 import { TOKEN_ADDRESS } from "@/constants/constants";
 import { toast } from "react-toastify";
 import {
